f029947db9b3a26c9bdd760da242d1abe351b253
win20hun-collab <win20hun@gmail.com>
2025-11-13 00:48:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests, time
 
 # === 설정 부분 ===
-URL = "https://ko.aliexpress.com/item/1005010120015183.html?invitationCode=ZXRlQUNVNkpPRUd3N2lRNlhCUGcvcGVhbGdxWXhWTkNhckJpcnN5UXQ3MmVQemFTZUJrNWVWT0s1MU1hdTAyWg&srcSns=sns_KakaoTalk&sourceType=1&spreadType=socialShare&social_params=6000370900927&bizType=ProductDetail&spreadCode=ZXRlQUNVNkpPRUd3N2lRNlhCUGcvcGVhbGdxWXhWTkNhckJpcnN5UXQ3MmVQemFTZUJrNWVWT0s1MU1hdTAyWg&aff_fcid=b74c0bc49c3a45f29dd48b8dce73799d-1762955153404-07205-_c4DXaxzR&tt=MG&aff_fsk=_c4DXaxzR&aff_platform=default&sk=_c4DXaxzR&aff_trace_key=b74c0bc49c3a45f29dd48b8dce73799d-1762955153404-07205-_c4DXaxzR&shareId=6000370900927&businessType=ProductDetail&platform=AE&terminal_id=0aaa77ecea0643c7b35d9dd7cefd9ac9&afSmartRedirect=y&gatewayAdapt=glo2kor  # 감시할 상품 URL
+URL = "https://www.aliexpress.com/item/1005010120015183.html?invitationCode=ZXRlQUNVNkpPRUd3N2lRNlhCUGcvcGVhbGdxWXhWTkNhckJpcnN5UXQ3MmVQemFTZUJrNWVWT0s1MU1hdTAyWg&srcSns=sns_KakaoTalk&sourceType=1&spreadType=socialShare&social_params=6000370900927&bizType=ProductDetail&spreadCode=ZXRlQUNVNkpPRUd3N2lRNlhCUGcvcGVhbGdxWXhWTkNhckJpcnN5UXQ3MmVQemFTZUJrNWVWT0s1MU1hdTAyWg&aff_fcid=b74c0bc49c3a45f29dd48b8dce73799d-1762955153404-07205-_c4DXaxzR&tt=MG&aff_fsk=_c4DXaxzR&aff_platform=default&sk=_c4DXaxzR&aff_trace_key=b74c0bc49c3a45f29dd48b8dce73799d-1762955153404-07205-_c4DXaxzR&shareId=6000370900927&businessType=ProductDetail&platform=AE&terminal_id=0aaa77ecea0643c7b35d9dd7cefd9ac9&afSmartRedirect=y&gatewayAdapt=glo2kor  # 감시할 상품 URL
 CHECK_INTERVAL = 600  # 10분마다 확인 (초 단위)
 BOT_TOKEN = "8575667908:AAHxAxwEPBx30IWldmPc_EF9hEQLxp_H1o4"  # 텔레그램 봇 토큰
 CHAT_ID = "6646276816"  # 내 텔레그램 ID
